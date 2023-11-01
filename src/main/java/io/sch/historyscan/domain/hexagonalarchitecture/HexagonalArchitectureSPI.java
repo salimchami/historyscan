5abdd536a2990a58f9193a1d2161c9cb5dd88254
Salim CHAMI <it@salimchami.net>
2023-11-01 12:26:26 +0100

    network cloc and revisions

--- src/main/java/io/sch/historyscan/domain/hexagonalarchitecture/HexagonalArchitectureSPI.java
+++ src/main/java/io/sch/historyscan/domain/hexagonalarchitecture/HexagonalArchitectureSPI.java
@@ -3,6 +3,15 @@ package io.sch.historyscan.domain.hexagonalarchitecture;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Target;
 
+/**
+ * This annotation is used to mark a class as a SPI (Service Provider Interface) of the hexagonal architecture.
+ * <p>
+ * The hexagonal architecture is a way to structure your codebase in a way that the business logic is decoupled
+ * from the technical details. This is done by defining a core domain (the business logic) and a set of ports
+ * (interfaces) that the core domain can use to communicate with the outside world. The outside world can then
+ * implement these ports (adapters) to communicate with the core domain.
+ * </p>
+ */
 @Target(ElementType.TYPE)
 public @interface HexagonalArchitectureSPI {
 }
